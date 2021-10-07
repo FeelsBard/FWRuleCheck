f475815cd3d9749f610d1869da27d5a6cb26791e
FeelsBard <59395752+FeelsBard@users.noreply.github.com>
2021-10-07 12:18:43 -0400

    Update FWRuleCheck.ps1

--- FWRuleCheck.ps1
+++ FWRuleCheck.ps1
@@ -1,4 +1,1 @@
-$Rules = Get-NetFirewallRule -All | Where-Object {$profiles.sid -notcontains $_.owner -and $_.owner }
-$rules2 = Get-NetFirewallRule -All -PolicyStore ConfigurableServiceStore | Where-Object { $profiles.sid -notcontains $_.owner -and $_.owner }
-$total = $rules.count + $rules2.count
-Write-Host "Total" $total "Firewall Rules"
+Get-AppXPackage -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
